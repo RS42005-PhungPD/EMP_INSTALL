e923fd6f2ca286d49e4487b46d1768e2be307603
LongAudi <89772391+LongAudi@users.noreply.github.com>
2025-12-24 09:14:07 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -451,6 +451,10 @@ version: "3.9"
 services:
   system_info:
     image: ${SOURCE_IMAGE}
+
+    stop_signal: SIGINT
+    stop_grace_period: 5s
+    
     environment:
       - HOST_IP=${HOST_IP}
       - HOST_SSH_PORT=${HOST_SSH_PORT}
